941530593cdbd2789f637efec7746987aba116c2
luidy <luidy@gmail.com>
2020-03-22 13:37:56 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/chat/ramos/com/br/loja_virtual_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package chat.ramos.com.br.loja_virtual_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:loja_virtual_app/tabs/home_tab.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/tabs/products_tab.dart';
+import 'package:loja_virtual_app/widgets/custom_drawer.dart';
+import 'package:loja_virtual_app/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        )
+      ],
+    );
+  }
+}
